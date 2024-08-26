f66b509f6c78453eb85bb69d0a7353172523dba7
Matt <matthewthaskins@gmail.com>
2024-08-26 23:24:10 +0100

    Added Index File

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import {Button} from "./Button/Button.jsx";
+import {Header} from '/Header/Header.jsx';
+import {LoginPage} from './LoginPage/LoginPage.jsx';
+import {RegisterPage} from "./RegisterPage/RegisterPage.jsx";
+import {TextInput} from "./TextInput/TextInput.jsx";
+import {ToolboxCard} from "./ToolboxCard/ToolboxCard.jsx";
+
+export {
+    Button, Header, LoginPage, RegisterPage, TextInput, ToolboxCard
+
+}
